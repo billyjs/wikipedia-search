9e2e808195d7b4c03695c756054173a32c7ffc8a
Billy Schulze <billyschulze97@gmail.com>
2017-03-28 20:44:44 +1000

    added initial site code

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
 /**
  * Created by billy on 28/03/17.
  */
+var github = "https://www.github.com/billy-7";
+var linkedin = "https://www.linkedin.com/in/billyschulze";
+
+var searchlimit = 7;
+var api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=' +
+    searchlimit + '&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+var cb = '&callback=?';
+var url = "https://en.wikipedia.org/?curid=";
+
+var result = ['<div class="result"><div class="content"><h2>', '</h2><p>', '</p></div><i id="', '" class="fa fa-arrow-right" aria-hidden="true"></i></div>'];
+
+function search() {
+    var searchterm = $("#searchbox").val();
+    $.getJSON(api + searchterm + cb, function (data) {
+        var pages = data.query.pages;
+        var ids = Object.keys(pages);
+        $(".result").remove();
+        ids.forEach(function (id) {
+            $(".container").append(result[0] + pages[id].title + result[1] + pages[id].extract + result[2] + id + result[3]);
+        });
+        $(".fa-arrow-right").click(function () {
+            window.open(url + $(this).attr("id"));
+        });
+
+    }).fail(function () {
+        console.log("error");
+    });
+};
+
+$(function() {
+    $(".btn-search").click(search);
+    $('#searchbox').on('keydown', function (key) {
+        if (key.which == 13) {
+            key.preventDefault();
+            search();
+        }
+    });
+    $(".btn-random").click(function () {
+        window.open("https://en.wikipedia.org/wiki/Special:Random");
+    });
+
+    $(".github-tab").click(function () {
+        window.open(github);
+    });
+
+    $(".linkedin-tab").click(function () {
+        window.open(linkedin);
+    });
+
+    $("#searchbox").focus();
+});
